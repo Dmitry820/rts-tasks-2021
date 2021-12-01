48152877443a8bcf4eeb95457776348a40e4de04
Dmitry820 <54149894+Dmitry820@users.noreply.github.com>
2021-12-01 13:18:11 +0300

    Update task03.ino
    Kurilo and Gubaidulin

--- task03/task03.ino
+++ task03/task03.ino
@@ -1,7 +1,33 @@
-void setup() {
+const int enablePin = 8; 
+const int stepPin = 3; 
+const int dirPin = 6; 
 
+void setup() {
+  pinMode(enablePin, OUTPUT);
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+  digitalWrite(enablePin, LOW);
 }
 
 void loop() {
+  digitalWrite(dirPin, HIGH); 
+  for (int x = 0; x < 300; x++) {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(300);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(300);
+}
+delay(1000);
 
+digitalWrite(dirPin, LOW);
+ for (int x = 0; x < 300; x++) {
+  digitalWrite(stepPin, HIGH);
+  delayMicroseconds(300);
+  digitalWrite(stepPin, LOW);
+  delayMicroseconds(300);
+}
+delay(1000);
+digitalWrite(enablePin, HIGH);
+delay(1000);
+digitalWrite(enablePin, LOW);
 }
